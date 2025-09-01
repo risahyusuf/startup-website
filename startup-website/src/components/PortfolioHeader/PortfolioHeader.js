b77c76dede803c8c0b832dcdff74a3c79ab88f69
risahyusuf <risahyusuf@gmail.com>
2025-09-01 15:39:11 +0100

    portfolioheader complete

--- startup-website/src/components/PortfolioHeader/PortfolioHeader.js
+++ startup-website/src/components/PortfolioHeader/PortfolioHeader.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import "./PortfolioHeader.css"
 
 const PortfolioHeader = () => {
   return (
-    <div>PortfolioHeader</div>
+    <section className="portfolioheader">
+      <h1>W O R K S</h1>
+      <h2>Portfolio</h2>
+      <p>Lorem ipsum, dolor sit amet consectetur<br />
+         <span className="linebreak">adipisicing elit.</span></p>
+         </section>
   )
 }
 
